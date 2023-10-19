ba6b2ec48b91f15e69cd515a5df3c72345bf1edc
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2023-10-19 12:51:56 +0900

    modified:   desktop/src/App.js

--- desktop/src/App.js
+++ desktop/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import Amplify from 'aws-amplify';
 import awsExports from './aws-exports';
 
