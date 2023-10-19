36ff9e16353873579ed3212910098e6dba9dbe58
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2023-10-19 12:12:30 +0900

    modified:   desktop/src/App.js

--- desktop/src/App.js
+++ desktop/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Amplify } from 'aws-amplify';
+import { Amplify } from '@aws-amplify';
 import awsExports from './aws-exports';
 import { Flex, withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
