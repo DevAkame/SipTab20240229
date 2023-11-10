0823783753710f1eab4460b3a02360875bf90b35
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2023-11-10 09:15:56 +0900

    modified:   src/App.js

--- desktop/src/App.js
+++ desktop/src/App.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Auth, Hub } from 'aws-amplify';
+import { Amplify, Auth, Hub } from 'aws-amplify';
 import { FirthSetUpProfiles, NavBarHeader } from './ui-components';
-import { Flex, withAuthenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator, useAuthenticator, Flex } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
 import { API, graphqlOperation } from 'aws-amplify';
 import { getUserProfiles } from './graphql/queries';
+
+Amplify.configure(awsExports);
 
 function App() {
   const [user, setUser] = useState(null);
@@ -51,16 +55,15 @@ function App() {
               } else {
                 return <FirthSetUpProfiles />;
               }
-            })};
+            })}
             <p>testnandayo</p>
           </div>
         ) : (
           <p>Not Authenticated</p>
-        )};
+        )}
       </Flex>
     </div>
   );
 }
-
 
 export default withAuthenticator(App);
