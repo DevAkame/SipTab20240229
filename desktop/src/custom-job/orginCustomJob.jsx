e2b8f38bec8bad84edd6eabbce79f0a2284a6ce6
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2023-11-11 16:13:41 +0900

    modified:   src/custom-job/orginCustomJob.jsx

--- desktop/src/custom-job/orginCustomJob.jsx
+++ desktop/src/custom-job/orginCustomJob.jsx
@@ -4,12 +4,6 @@
 - CheckUserProfile(arg01) ユーザプロファイル登録チェック
 
 */
-
-
-// 概要：ユーザプロファイルテーブルに対象ユーザのレコードがあるかの判定を行う。
-// 利用方法：CheckUserProfile(String(【ユーザID】)) ※user.attributes.subとか
-// 戻り値：　bool(ユーザ有り＝正)
-
 import React from 'react';
 import { Amplify} from 'aws-amplify';
 import { API, graphqlOperation } from 'aws-amplify';
@@ -18,10 +12,13 @@ import { getSex } from '../graphql/queries';
 
 Amplify.configure(awsExports);
 
+// 概要：ユーザプロファイルテーブルに対象ユーザのレコードがあるかの判定を行う。
+// 利用方法：CheckUserProfile(String(【ユーザID】)) ※user.attributes.subとか
+// 戻り値：　bool(ユーザ有り＝正)
 export function CheckUserProfile(arg01){
     const userProfilesData = null;
-    API.graphql(graphqlOperation(getSex)).then((values) => {
-        const userProfilesData = values.data.getUserProfiles.items;
+    API.graphql(graphqlOperation(listStockDataPools)).then((values) => {
+        const userProfilesData = values.data.listStockDataPools.items;
         return ( <p>"testtstt"</p> )
     })
 };
