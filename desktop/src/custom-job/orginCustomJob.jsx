f0f7a4983ea8a29986f86e032166997d80993917
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2023-11-11 15:47:25 +0900

    modified:   src/custom-job/orginCustomJob.jsx

--- desktop/src/custom-job/orginCustomJob.jsx
+++ desktop/src/custom-job/orginCustomJob.jsx
@@ -1,3 +1,21 @@
+/*
+インポート文：import { 変数名 } from './custom-job/orginCustomJob';
+変数一覧：
+- CheckUserProfile(arg01) ユーザプロファイル登録チェック
+
+*/
+
+
+// 概要：ユーザプロファイルテーブルに対象ユーザのレコードがあるかの判定を行う。
+// 利用方法：CheckUserProfile(String(【ユーザID】)) ※user.attributes.subとか
+// 戻り値：　bool(ユーザ有り＝正)
+
+import React from 'react';
+import { Amplify} from 'aws-amplify';
+import { API, graphqlOperation } from 'aws-amplify';
+import awsExports from '../aws-exports';
+
+Amplify.configure(awsExports);
 
 export function CheckUserProfile(arg01){
     return (<p>{arg01}</p>)
