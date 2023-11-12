7ff0e09c964240b078d1a2703d44ef4af1d43d64
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2023-11-12 15:32:11 +0900

    modified:   src/custom-job/orginCustomJob.jsx

--- desktop/src/custom-job/orginCustomJob.jsx
+++ desktop/src/custom-job/orginCustomJob.jsx
@@ -18,7 +18,7 @@ Amplify.configure(awsExports);
 export function CheckUserProfile(arg01){
     API.graphql(graphqlOperation(listSexes)).then((values) => {
         const data = values.data.listSexes.items;
-        console.log(toString(data));
+        console.log(toString(values.data?));
         return ( <p>{data}</p> )
     })
 };
