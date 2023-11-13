68ba412e5e06347d8faa09891724977059c1cbad
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2023-11-13 09:32:42 +0900

    modified:   src/App.js 	modified:   src/custom-job/orginCustomJob.jsx

--- desktop/src/App.js
+++ desktop/src/App.js
@@ -49,7 +49,7 @@ function App() {
 
             <p>{user.attributes.sub}</p>
             <p>testnandayo</p>
-            {CheckUserProfile(String(user.attributes.sub))};
+            {CheckUserProfile(String(user.attributes.sub))}
           </div>
         ) : (
           <p>Not Authenticated</p>
