f9855d621825e015f189918814e05718c3f6f955
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2023-12-06 12:56:28 +0900

    new file:   public/img/siptabIcon.png 	new file:   src/custom-components/MainNavigate.jsx

---
+++ desktop/src/custom-components/MainNavigate.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import React from "react";
+
+
+export function MainHeader() {
+
+    return (
+        <>
+            <img src={`${process.env.PUBLIC_URL}/img.siptabIcon.png`} alt="SipTabLogo" />
+        </>
+    )
+    }
+}
