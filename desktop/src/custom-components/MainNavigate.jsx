d0232c5f4eee16d8352a37cf2d3fbc6064b4a6c0
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2023-12-08 16:20:46 +0900

    modified:   src/custom-components/MainNavigate.jsx

--- desktop/src/custom-components/MainNavigate.jsx
+++ desktop/src/custom-components/MainNavigate.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import React from "react";
+import {Flex, SelectField} from "@aws-amplify/ui-react"
 
 
 export function MainHeader() {
 
     return (
-        <>
+        <Flex direction="column">
             <img src={`${process.env.PUBLIC_URL}/img/siptabIcon.png`} alt="SipTabLogo" />
-        </>
+            <SelectField options={["All","Cherish Premium"]}></SelectField>
+            <h2>Akame</h2>
+            <button onClick={() => alert('test')}>SignOut</button>
+        </Flex>
     )
 }
