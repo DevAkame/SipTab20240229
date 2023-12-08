6056bce4dd9fe8e7be715ad0e81da21acd0c02b3
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2023-12-08 10:47:27 +0900

    modified:   src/custom-components/MainNavigate.jsx

--- desktop/src/custom-components/MainNavigate.jsx
+++ desktop/src/custom-components/MainNavigate.jsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
 import React from "react";
+import { SelectField,Flex } from "@aws-amplify/ui-react";
 
 
 export function MainHeader() {
-    const imgStyle = {
-        width: 20px;
-        height:20px;
-    }
+
 
 
     return (
         <>
-            <img src={`${process.env.PUBLIC_URL}/img/siptabIcon.png`} alt="SipTabLogo" style={imgStyle}/>
+            <img src={`${process.env.PUBLIC_URL}/img/siptabIcon.png`} alt="SipTabLogo" />
         </>
     )
 }
