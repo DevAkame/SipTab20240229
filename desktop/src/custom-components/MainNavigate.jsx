d38a0cb84a421e3bf522261a3210b77e7984b609
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2023-12-09 14:42:22 +0900

    modified:   src/custom-components/MainNavigate.jsx

--- desktop/src/custom-components/MainNavigate.jsx
+++ desktop/src/custom-components/MainNavigate.jsx
@@ -1,16 +1,30 @@
-import { useState } from "react";
-import React from "react";
+import * as React from "react";
+import { useAuth } from "@aws-amplify/ui-react/internal";
+import { getOverrideProps, useAuthSignOutAction } from "./utils";
 import {Flex, SelectField} from "@aws-amplify/ui-react"
 
 
 export function MainHeader(props) {
+    const { overrides, ...rest } = props;
+    const authAttributes = useAuth().user?.attributes ?? {};
+    const buttonOnClick = useAuthSignOutAction({ global: false });
     
     return (
         <Flex direction="column">
             <img src={`${process.env.PUBLIC_URL}/img/siptabIcon.png`} alt="SipTabLogo" />
             <SelectField options={["All","Cherish Premium"]}></SelectField>
             <h2>{props.name}</h2>
-            <button onClick={() => alert('test')}>SignOut</button>
+            <button
+            width="unset"
+            height="unset"
+            shrink="0"
+            size="small"
+            isDisabled={false}
+            variation="destructive"
+            children="Sign Out"
+            onClick={() => {
+              buttonOnClick()}}
+              {...getOverrideProps(overrides, "Button")}></button>
         </Flex>
     )
 }
