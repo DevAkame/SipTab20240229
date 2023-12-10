2effd3c47eb7fdc16076d064f1606da8ba7a17f0
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2023-12-11 04:05:04 +0900

    modified:   src/custom-components/MainNavigate.jsx

--- desktop/src/custom-components/MainNavigate.jsx
+++ desktop/src/custom-components/MainNavigate.jsx
@@ -2,24 +2,39 @@ import * as React from "react";
 import { useAuth } from "@aws-amplify/ui-react/internal";
 import { getOverrideProps, useAuthSignOutAction } from "../ui-components/utils";
 import {Flex, SelectField,Text} from "@aws-amplify/ui-react"
+import { createUseStyles } from 'react-jss';
+
+const useStyle = createUseStyles({
+    styleIconImg:{
+        width: 50px,
+        height: 50px,
+        display: block,
+        direction: column,
+        alignSelf: Stretch,
+        position: SpeechRecognitionAlternative,
+        padding: 0
+    }
+})
 
 export function MainHeader(props) {
     const { overrides, ...rest } = props;
     const authAttributes = useAuth().user?.attributes ?? {};
     const buttonOnClick = useAuthSignOutAction({ global: false });
-    
+    const classes = useStyle();
     return (
         <Flex direction="column">
             <img
-            width="100px"
-            height="100px"
+            className={classes.styleIconImg}
+            src={`${process.env.PUBLIC_URL}/img/siptabIcon.png`} alt="SipTabLogo" />
+            <SelectField 
+            width="unset"
+            height="unset"
             display="block"
             direction="column"
             alignSelf="stretch"
             position="relative"
             padding="0px 0px 0px 0px"
-            src={`${process.env.PUBLIC_URL}/img/siptabIcon.png`} alt="SipTabLogo" />
-            <SelectField options={["All","Cherish Premium"]}></SelectField>
+            options={["All","Cherish Premium"]}></SelectField>
             <Text
             fontFamily="Inter"
             fontSize="16px"
