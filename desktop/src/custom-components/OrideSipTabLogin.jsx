d4f11e371938ddc50ea34c0b3f1c2083a426e129
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2023-12-29 16:54:44 +0900

    new file:   src/custom-components/OrideSipTabLogin.jsx

---
+++ desktop/src/custom-components/OrideSipTabLogin.jsx
@@ -0,0 +1,16 @@
+import { Flex } from "@aws-amplify/ui-react";
+import { SipTabSignIn,SipTabSignInOrCreateAcountChangeBar} from "../ui-components";
+
+
+
+export function SipTabLoginView(){
+    return(
+        <Flex direction="row"
+        width="50vw"
+        height="100%"
+        padding="auto">
+            <SipTabSignInOrCreateAcountChangeBar />
+            <SipTabSignIn />
+        </Flex>
+    )
+}
