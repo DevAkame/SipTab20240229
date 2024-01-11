49fb469b7447d672f1b468a2b4840487837a1bf4
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2024-01-11 16:43:08 +0900

    modified:   src/custom-components/STLoginViews.jsx

--- desktop/src/custom-components/STLoginViews.jsx
+++ desktop/src/custom-components/STLoginViews.jsx
@@ -11,39 +11,70 @@ export function STLoginViews(props) {
   
   return (
     <Flex
-      gap="10px"
-      direction="row"
-      width="unset"
-      height="unset"
-      justifyContent="flex-start"
-      alignItems="flex-start"
-      overflow="hidden"
-      position="relative"
-      padding="10px 10px 10px 10px"
-      backgroundColor="rgba(255,255,255,1)"
-      {...getOverrideProps(overrides, "STSignInOrCreateAcountChangeBar")}
-      {...rest}
-    >
-      <Button
-        width="150px"
-        height="unset"
-        shrink="0"
-        size="small"
-        isDisabled={false}
-        variation="link"
-        children="SingIn"
-        {...getOverrideProps(overrides, "SIBtn")}
-      onClick={() => btnhandlers(props)} ></Button>
-      <Button
-        width="150px"
-        height="unset"
-        shrink="0"
-        size="small"
-        isDisabled={false}
-        variation="link"
-        children="CreateAcount"
-        {...getOverrideProps(overrides, "CAcountBtn")}
-      ></Button>
+        direction="column"
+        width="50vw"
+        height="100%"
+        gap="0rem"
+        justifyContent="center"
+        alignItems="center"
+        paddingTop="100px"
+        >
+            <Flex
+            gap="10px"
+            direction="row"
+            width="unset"
+            height="unset"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            overflow="hidden"
+            position="relative"
+            padding="10px 10px 10px 10px"
+            backgroundColor="rgba(255,255,255,1)"
+            {...getOverrideProps(overrides, "STSignInOrCreateAcountChangeBar")}
+            {...rest}
+            >
+                <Button
+                    width="150px"
+                    height="unset"
+                    shrink="0"
+                    size="small"
+                    isDisabled={false}
+                    variation="link"
+                    children="SingIn"
+                    {...getOverrideProps(overrides, "SIBtn")}
+                    onClick={() => btnhandlers(props)} 
+                >
+                </Button>
+                <Button
+                  width="150px"
+                  height="unset"
+                  shrink="0"
+                  size="small"
+                  isDisabled={false}
+                  variation="link"
+                  children="CreateAcount"
+                  {...getOverrideProps(overrides, "CAcountBtn")}
+                ></Button>
+            </Flex>
+
+            <SipTabSignIn 
+                width="400px"
+            />
+            <SipTabCreateAccount
+                width="400px"
+                display="none"
+            />
+            <SipTabForgetPassword
+                width="400px"
+                display="none"
+            />
+
     </Flex>
+
+
+
+
+
+    
   );
 }
