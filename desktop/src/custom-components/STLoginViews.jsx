67d79e08548f68456ef60f9fc81547657309b099
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2024-01-16 13:57:36 +0900

    modified:   src/custom-components/STLoginViews.jsx

--- desktop/src/custom-components/STLoginViews.jsx
+++ desktop/src/custom-components/STLoginViews.jsx
@@ -22,7 +22,7 @@ export function STLoginViews(props) {
         return (
             overrides={
                  "SIBtn":{
-                    isDisabled : {false}
+                    isDisabled : false
                  }
             }
         )
