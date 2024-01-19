78d079aafd76fc1f171378cdbbe30c39e805baa6
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2024-01-19 21:06:37 +0900

    modified:   src/custom-components/STLoginViews.jsx

--- desktop/src/custom-components/STLoginViews.jsx
+++ desktop/src/custom-components/STLoginViews.jsx
@@ -42,9 +42,10 @@ export function STLoginViews(props) {
             ViewSignIn.style.display = "block";
             ViewCreateAcunt.style.display = "none";
 
-        }
+        };
     
     };
+
   return (
     <Flex
         direction="column"
@@ -97,7 +98,130 @@ export function STLoginViews(props) {
                 onClick={BtnChangeCompSgnOrCrateAccount} 
             ></Button>
         </Flex>
-
+{/* ログイン　*/}
+        <Flex
+            id="SignInView"
+            gap="35px"
+            direction="column"
+            width="500px"
+            height="426px"
+            justifyContent="flex-start"
+            alignItems="center"
+            overflow="hidden"
+            position="relative"
+            padding="10px 8px 10px 8px"
+            backgroundColor="rgba(255,255,255,1)"
+            {...getOverrideProps(overrides, "SipTabSignIns")}
+            {...rest}
+        >
+            <TextField
+                width="300px"
+                height="unset"
+                label="Email"
+                shrink="0"
+                placeholder="Mail Address"
+                size="small"
+                isDisabled={false}
+                labelHidden={false}
+                variation="default"
+                {...getOverrideProps(overrides, "SignInMail")}
+            ></TextField>
+            <PasswordField
+                width="300px"
+                height="unset"
+                label="Password"
+                shrink="0"
+                placeholder="password"
+                size="small"
+                isDisabled={false}
+                labelHidden={false}
+                variation="default"
+                hideShowPassword={false}
+                {...getOverrideProps(overrides, "SignInPasswd")}
+            ></PasswordField>
+            <Button
+                width="300px"
+                height="unset"
+                padding="3px 0px 3px 0px"
+                shrink="0"
+                size="small"
+                isDisabled={false}
+                variation="primary"
+                children="Confirm"
+                {...getOverrideProps(overrides, "SignInSubmitBtn")}
+            ></Button>
+        </Flex>
+{/* アカウント新規作成画面 */}
+        <Flex
+            id="CreateAcountView"
+            display="none"
+            gap="19px"
+            direction="column"
+            width="500px"
+            height="unset"
+            justifyContent="flex-start"
+            alignItems="center"
+            overflow="hidden"
+            position="relative"
+            padding="31px 47px 31px 47px"
+            backgroundColor="rgba(255,255,255,1)"
+            {...getOverrideProps(overrides, "SipTabCreateAccount")}
+            {...rest}
+        >
+            <TextField
+                width="300px"
+                height="unset"
+                label="Email"
+                shrink="0"
+                placeholder="Placeholder"
+                size="small"
+                variation="default"
+                {...getOverrideProps(overrides, "CreateAccountEmailField")}
+            ></TextField>
+            <TextField
+                width="300px"
+                height="unset"
+                label="Name"
+                shrink="0"
+                placeholder="Placeholder"
+                size="small"
+                variation="default"
+                {...getOverrideProps(overrides, "CreateAccountNameField")}
+            ></TextField>
+            <PasswordField
+                width="300px"
+                height="unset"
+                label="Password"
+                shrink="0"
+                placeholder="Placeholder"
+                size="small"
+                variation="default"
+                hideShowPassword={false}
+                {...getOverrideProps(overrides, "CreateAccountPasswordField")}
+            ></PasswordField>
+            <PasswordField
+                width="300px"
+                height="unset"
+                label="Confirm Password"
+                shrink="0"
+                placeholder="Placeholder"
+                size="small"
+                variation="default"
+                hideShowPassword={false}
+                {...getOverrideProps(overrides, "CreateAccountConfirmPasswordField")}
+            ></PasswordField>
+            <Button
+                width="unset"
+                height="unset"
+                padding="3px 0px 3px 0px"
+                shrink="0"
+                alignSelf="stretch"
+                size="default"
+                variation="primary"
+                children="Confirm"
+                {...getOverrideProps(overrides, "CreateAccountConfirmBtn")}
+            ></Button>
+        </Flex>
     </Flex>
   );
-};
+}
