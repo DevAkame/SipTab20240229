4dab3d09f15c091c13984eb7c409450e6be87a7d
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2024-01-27 16:39:57 +0900

    new file:   src/ui-lib/common/Foods.jsx 	new file:   src/ui-lib/login/MainLogin.jsx 	new file:   src/ui-lib/login/auth/LoginAuth.jsx

---
+++ desktop/src/ui-lib/common/Foods.jsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import { getOverrideProps } from "../../ui-components/utils";
+import { Button, Flex, View } from "@aws-amplify/ui-react";
+
+
+export default function Foods(props){
+    const {overrides, ...rest } = props;
+    return(
+        <Flex
+          gap="2px"
+          direction="row"
+          width="100%"
+          height="unset"
+          alignSelf="stretch"
+          justifyContent="center"
+          alignItems="center"
+          position="relative"
+          padding="0px"
+          backgroundColor="rgba(255,255,255,1)"
+        >
+            <Button
+              width="100%"
+              height="unset"
+              shrink="0"
+              size="small"
+              isDisabled={false}
+              variation="link"
+            >
+                <Image
+                  width="20px"
+                  height="15.9px"
+                  display="block"
+                  position="relative"
+                  padding="0px"
+                  objectFit="cover"
+                  src={`${process.env.PUBLIC_URL}/img/common/akame.png`}
+                ></Image>
+
+                <View
+                  as="p"
+                  width="unset"
+                  height="unset"
+                  fontSize="small"
+                  color="rgba(0,0,0,10)"
+                >
+                    Akame
+                </View>
+            </Button>
+        </Flex>
+
+
+    )
+}
---
+++ desktop/src/ui-lib/login/MainLogin.jsx
@@ -0,0 +1,18 @@
+import { Flex, View } from "@aws-amplify/ui-react";
+
+export default function LoginView(prop){
+
+    return(
+        <View
+          as="div"
+          width="100vw"
+          height="100vh"
+          backgroundColor="rgba(170,170,191,0.15)"
+        >
+            <Flex 
+              direction="row"
+            >
+            </Flex>
+        </View>
+    )
+}
