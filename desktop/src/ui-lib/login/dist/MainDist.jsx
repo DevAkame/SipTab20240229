740afb8f3320f8973c6bbba6aa29221a1e53bc8c
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2024-01-29 15:34:20 +0900

    modified:   src/ui-lib/login/dist/MainDist.jsx

--- desktop/src/ui-lib/login/dist/MainDist.jsx
+++ desktop/src/ui-lib/login/dist/MainDist.jsx
@@ -10,37 +10,37 @@ export default function  DistView(prop){
           height="100vh"
           backgroundColor="rgba(170,170,191,0.15)"
         >
-            <Flex
-              direction="column"
-              justifyContent="flex-end"
-              >
-              <View
-                as="div"
+          <Flex
+            direction="column"
+            justifyContent="flex-end"
+          >
+            <View
+              as="div"
+              width="100%"
+              height="unset"
+              backgroundColor="rgba(255,170,191,0.15)"
+            >
+              <Flex
+                direction="column"
+                justifyContent="start"
                 width="100%"
                 height="unset"
-                backgroundColor="rgba(255,170,191,0.15)"
               >
-                <Flex
-                  direction="column"
-                  justifyContent="start"
-                  width="100%"
-                  height="unset"
+                <View
+                  as="p"
+                  backgroundColor="rgba(255,170,255,0.15)"
+
                 >
-                  <View
-                    as="p"
-                    backgroundColor="rgba(255,170,255,0.15)"
+                業務統合管理アプリSipTabへようこそ<br />
+                　当アプリのご利用方法ご確認の場合は下記を参照ください。
+                </View>
 
-                  >
-                  業務統合管理アプリSipTabへようこそ<br />
-                  　当アプリのご利用方法ご確認の場合は下記を参照ください。
-                  </View>
-
-                </Flex>
+              </Flex>
 
                 
-              </View>
-              <Foods />
-            </Flex>
+            </View>
+            <Foods />
+          </Flex>
         
         </View>
 
