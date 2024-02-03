4934e190b0c905779915340b191b4060aab3ee8d
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2024-02-03 15:05:29 +0900

    new file:   src/ui-lib/home/MainHome.jsx

---
+++ desktop/src/ui-lib/home/MainHome.jsx
@@ -0,0 +1,10 @@
+import { View } from "@aws-amplify/ui-react";
+import * as React from "react";
+
+export default function MainHomeViews(props){
+    return(
+        <View>test1</View>
+        
+    );
+
+};
