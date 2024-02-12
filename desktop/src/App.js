0b3f45a46eb3befccb3b574b0d006b601f7b544e
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2024-02-12 15:25:43 +0900

    modified:   src/App.js 	modified:   src/index.js

--- desktop/src/App.js
+++ desktop/src/App.js
@@ -2,20 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { Amplify, Auth, Hub } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
-
-// router 
-import { Route,Navigate, Routes} from 'react-router-dom';
-
-// imported Compornent
-import LoginView from './ui-lib/login/MainLogin';
-import MainHomeViews from './ui-lib/home/MainHome';
-import { Button, View } from '@aws-amplify/ui-react';
+import { Button } from '@aws-amplify/ui-react';
 
 
 Amplify.configure(awsExports);
 
 
-function PreApp() {
+function App() {
   const [count,setCount] = useState(0);
   return(
 
@@ -34,44 +27,6 @@ onClick={()=> setCount(count + 1)}
   );
 };
 
-function App () {
-  const [user,setUser ] = useState(null);
-
-
-  useEffect(() => {
-    const fetchUserData = async () =>{
-      try {
-        console.log("test1");
-        setUser(await Auth.currentAuthenticatedUser());
-        console.log("test2");
-      } catch (error){
-        console.log(error);
-        setUser(null);
-      }
-    };
-  },[]);
-
-  Hub.listen('auth',(data) =>{
-    const payload = data;
-    console.log(payload.event);
-    if (payload.event === 'signOut'){
-      setUser(null);
-    };
-  });
-
-  return(
-    <Routes>
-      <Route path='/login' element={<LoginView /> } />
-      <Route path='/' element={user ? <MainHomeViews /> : <Navigate replace to="/login" />} />
-
-    </Routes>
-   
-
-  )
-};
-
-//export default App;
-// test
-export default PreApp;
+export default App;
 
 
