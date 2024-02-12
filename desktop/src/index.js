aaba5b69a53b78a15a467d195b2b5a5cbaa6160c
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2024-02-12 14:11:24 +0900

    modified:   src/index.js

--- desktop/src/index.js
+++ desktop/src/index.js
@@ -6,15 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import PreApp from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
-  {/*<Provider store={store}>*/}
-    {/*<BrowserRouter>*/}
-      <App />
-    {/*</BrowserRouter>*/}
-  {/*</Provider>*/}
+  <Provider store={store}>
+    <BrowserRouter>
+      <PreApp />
+    </BrowserRouter>
+  </Provider>
 );
 
 reportWebVitals();
