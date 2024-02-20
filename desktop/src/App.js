f051ff412640c6aeb4f76e05c60197d71ec12589
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2024-02-20 09:55:32 +0900

    modified:   src/App.js 	modified:   src/redux/userSlice.js

--- desktop/src/App.js
+++ desktop/src/App.js
@@ -2,28 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { Amplify, Auth, Hub } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
-import { Button } from '@aws-amplify/ui-react';
+import { Button, View } from '@aws-amplify/ui-react';
 
 
 import { useDispatch, useSelector } from 'react-redux';
-import { getSession } from './redux/userSlice';
+import { getSession, getUsers } from './redux/userSlice';
 
 Amplify.configure(awsExports);
 
 
 function App() {
-  const {hello,loading,error} = useSelector((state) => state.hello);
+  const { users, loading, error } = useSelector((state) => state.users);
 
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getSession());
+    dispatch(getUsers());
     console.log(hello);
-  },);
+  },[dispatch]);
 
 
   return(
 
-
+<>
+<View
+as='div'>{users.map((user, index) => <div key={index}>{user.name}</div>)}</View>
 <Button
 id="ActivateCreateAccount"
 width="50%"
@@ -32,10 +34,11 @@ shrink="0"
 size="small"
 disabled={false}
 variation="link"
-onClick={getSession()}
+onClick={getUsers()}
 backgroundColor="rgba(255,255,255,1)"
 >"test"</Button>
 
+</>
   );
 };
 
--- desktop/src/redux/userSlice.js
+++ desktop/src/redux/userSlice.js
@@ -1,26 +1,31 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { useReducer } from "react";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const urlHello = 'https://yf3f8gsib9.execute-api.ap-northeast-1.amazonaws.com/default/userauth-staging'
-
-export const getSession = createAsyncThunk('user/getSession',async () => {
-    return await fetch(urlHello).then((res) => res.json());
-    
+export const getUsers = createAsyncThunk('users/getUsers', async () => {
+  return await fetch('https://jsonplaceholder.typicode.com/users').then((res) =>
+    res.json()
+  );
 });
 
-const userSlice = createSlice({
-    name:'user',
-    initialState:{
-        session:[],
+const usersSlice = createSlice({
+  name: 'users',
+  initialState: {
+    users: [],
+    loading: false,
+    error: false,
+  },
+  extraReducers: {
+    [getUsers.pending]: (state) => {
+      state.loading = true;
     },
-    extraReducers:{
-        [getSession.fulfilled]:(state,action) => {
-            state.session = action.payload;
-        },
+    [getUsers.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.users = action.payload;
     },
+    [getUsers.rejected]: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
 });
 
-export const userReducer = userSlice.extraReducers;
-
-
-
+export default usersSlice.reducer;
