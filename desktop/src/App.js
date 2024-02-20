f68396268de31fd680bc95abf7830554c465738f
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2024-02-20 11:53:22 +0900

    modified:   src/App.js

--- desktop/src/App.js
+++ desktop/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
-import Amplify from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import store from './redux/store';
 import {MainHomeViews} from './ui-lib/home/MainHome';
